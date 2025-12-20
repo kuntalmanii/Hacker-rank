efe0f369128f770c2bdd004b4d67e93fe8f77a31
Manish kuntal <kuntalmani12@gmail.com>
2025-12-20 11:47:38 +0530

    Add files via upload

---
+++ Sum of Two Numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main() {
+    #include <stdio.h>
+
+    int num1, num2, sum;
+   
+    scanf("%d %d", &num1, &num2);
+    sum = num1 + num2;
+    printf("The sum of %d and %d is %d.", num1, num2, sum);
+
+   return 0;
+}   
