c70383b72f82eb2622d5a04f558c810e94633192
Manish kuntal <kuntalmani12@gmail.com>
2025-12-20 11:47:56 +0530

    Add files via upload

---
+++ Half the Number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int num1;
+    scanf("%d",&num1);
+   float res,num2;
+    num2 = num1;
+    res =  (num2) /2;
+    printf("Half of %d is %.2f\n",num1,res);
+       
+    return 0;
+}
